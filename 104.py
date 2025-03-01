53efdecea337e506690b29310391ff40189df998
t480 <vito0fasulo@gmail.com>
2025-03-01 20:15:37 +0100

    better version of 104

--- 104.py
+++ 104.py
@@ -8,43 +8,29 @@ def pad(s):
   return s + (BLOCK - len(s) % BLOCK) * chr(BLOCK - len(s) % BLOCK)
 
 def send_enc(conn, s):
-    conn.recvuntil("encrypt:")
+    print(conn.recvuntil("encrypt:"))
     conn.sendline(s)
-    txt = conn.recvline()
+    txt = conn.recvline().strip()
+    print(txt.decode())
     enc = txt.decode()[39:]
     return enc
 
 conn = remote("padding.challs.cyberchallenge.it", 9030)
 
 s = ""
-dec = ""
+flag = ""
 
-s += "a"
-enc = send_enc(conn, s)
-
-
-for x1 in range(16):
-    enc_test = send_enc(conn, pad("}"))
-    print(enc)
-    print(enc[-33:])
-    print(enc_test)
-    print(enc_test[:32])
-    if enc_test[:32] == enc[-33:]:
-        print(f"New flag char found")
-
-
-
-for _ in range(16):
-    s += "a"
-    enc = send_enc(conn, s)
+for i in range(16):
+    s = "a" * (15 - i)
+    enc = send_enc(conn, (s + flag).encode())
 
     for c in printable:
-        print(f"trying {c} ...")
-        enc_test = send_enc(conn, pad(c))
+        plain = s + flag + c
+        print(f"trying {plain} ...")
+        enc_test = send_enc(conn, plain.encode())
         print(enc)
-        print(enc[-33:])
         print(enc_test)
-        print(enc_test[:32])
-        if enc_test[:32] == enc[-33:]:
-            print(f"New flag char found: {c}")
+        if enc_test[:32] == enc[:32]:
+            print(f"New flag char found: {c}, flag={c + flag}")
+            flag = c + flag
             break
