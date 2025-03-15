21ebcbd2d7b9f51efbd306d1ca8bea55cc4993cd
vito4316 <vito0fasulo@gmail.com>
2025-03-15 11:24:00 +0100

    completed aes_lvl2

---
+++ ptm_crypto/aes_lvl2.py
@@ -0,0 +1,27 @@
+from pwn import *
+import base64
+
+def send_enc(conn, s):
+    print(f'Sending {s}')
+    conn.sendline(base64.b64encode(s))
+    enc = (conn.recvline().strip())
+    enc = base64.b64decode(enc[22:-1])
+    print(f'Received {enc}')
+    print(conn.recvline())
+    return enc
+
+r = remote("130.192.5.212", 1755) 
+
+print(r.recvline())
+print(r.recvline())
+
+enc = send_enc(r, b'00000000000000000000000000000000')
+
+enc1 = enc[0:16]
+enc2 = enc[16:32]
+
+zero_xor_a = [ (a ^ b) for a, b in zip(enc1, enc2) ]
+
+flag = "".join([ chr(a ^ b) for a, b in zip(zero_xor_a, b'00000000000000000000000000000000') ] )
+
+print( flag)
